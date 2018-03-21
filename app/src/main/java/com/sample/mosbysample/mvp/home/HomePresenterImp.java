421765533943ec7895a76d8dfa84a1ea9a965e1f
Shahanur <shahanur@surecash.net>
2018-03-21 13:21:46 +0600

    - Update presnter implementation architecture

--- app/src/main/java/com/sample/mosbysample/mvp/home/HomePresenterImp.java
+++ app/src/main/java/com/sample/mosbysample/mvp/home/HomePresenterImp.java
@@ -1,24 +1,28 @@
 package com.sample.mosbysample.mvp.home;
 
+import android.support.annotation.NonNull;
+
+import com.hannesdorfmann.mosby3.mvp.MvpBasePresenter;
 import com.sample.mosbysample.model.HomeModel;
 import com.sample.mosbysample.model.SuccessViewModel;
-import com.sample.mosbysample.mvp.core.MvpCorePresenterImp;
 
 /**
  * Created by Shahanur on 18-Mar-18.
  */
 
-public class HomePresenterImp extends MvpCorePresenterImp<HomeContract.View> implements HomeContract.Presenter {
+public class HomePresenterImp extends MvpBasePresenter<HomeContract.View> implements HomeContract.Presenter {
 
     @Override
     public void loadData() {
-        if (hasView()) {
-            getViewInterface().startLoad();
-            HomeModel model = new HomeModel();
-            model.setName("John");
-            model.setTitle("Mosby Test");
-            getViewInterface().showData(model, false);
-            getViewInterface().success(new SuccessViewModel());
-        }
+        ifViewAttached(view -> {
+            view.startLoad();
+        });
+        HomeModel model = new HomeModel();
+        model.setName("John");
+        model.setTitle("Mosby Test");
+        ifViewAttached(view -> {
+            view.showData(model, false);
+            view.success(new SuccessViewModel());
+        });
     }
 }
--- app/src/main/java/com/sample/mosbysample/mvp/transaction/TransactionListPresenterImp.java
+++ app/src/main/java/com/sample/mosbysample/mvp/transaction/TransactionListPresenterImp.java
@@ -9,16 +9,17 @@ import java.util.Random;
 import io.reactivex.Observable;
 import io.reactivex.Scheduler;
 import io.reactivex.disposables.Disposable;
+
+import com.hannesdorfmann.mosby3.mvp.MvpBasePresenter;
+import com.sample.mosbysample.model.ErrorViewModel;
 import com.sample.mosbysample.model.TransactionModel;
 import com.sample.mosbysample.model.SuccessViewModel;
-import com.sample.mosbysample.mvp.core.MvpCorePresenterImp;
 
 /**
  * Created by Shahanur on 19-Mar-18.
  */
 
-public class TransactionListPresenterImp extends MvpCorePresenterImp<TransactionListContract.View> implements TransactionListContract.Presenter {
-
+public class TransactionListPresenterImp extends MvpBasePresenter<TransactionListContract.View> implements TransactionListContract.Presenter {
 
     Scheduler asyncScheduler, uiScheduler;
     Disposable disposable;
@@ -30,30 +31,39 @@ public class TransactionListPresenterImp extends MvpCorePresenterImp<Transaction
 
     @Override
     public void loadData() {
-        getViewInterface().startLoad();
-        disposable = Observable.fromCallable(() -> loadListData())
+        ifViewAttached(view -> {
+            view.startLoad();
+        });
+        disposable = loadListData()
                 .observeOn(uiScheduler)
                 .subscribeOn(asyncScheduler)
                 .subscribe(items -> {
-                    if (hasView()) {
-                        getViewInterface().showData(items);
-                        getViewInterface().success(new SuccessViewModel());
-                    }
+                    ifViewAttached(view -> {
+                        view.showData(items);
+                        view.success(new SuccessViewModel());
+                    });
+                }, throwable -> {
+                    ifViewAttached(view -> {
+                        view.error(new ErrorViewModel(0, 0, true));
+                    });
                 });
     }
 
-    public List<TransactionModel> loadListData() {
-        SystemClock.sleep(5000);
-        Random random = new Random(30);
-        List<TransactionModel> listModels = new ArrayList<>();
-        for (int count = 0; count < 50; count++) {
-            TransactionModel model = new TransactionModel();
-            model.setAmount(random.nextInt(2000) + 50);
-            model.setTitle("Demo Name" + (count + 1));
-            model.setId(count);
-            listModels.add(model);
-        }
-        return listModels;
+    public Observable<List<TransactionModel>> loadListData() {
+        return Observable.fromCallable(() ->
+        {
+            SystemClock.sleep(5000);
+            Random random = new Random(30);
+            List<TransactionModel> listModels = new ArrayList<>();
+            for (int count = 0; count < 50; count++) {
+                TransactionModel model = new TransactionModel();
+                model.setAmount(random.nextInt(2000) + 50);
+                model.setTitle("Demo Name" + (count + 1));
+                model.setId(count);
+                listModels.add(model);
+            }
+            return listModels;
+        });
     }
 
     @Override
