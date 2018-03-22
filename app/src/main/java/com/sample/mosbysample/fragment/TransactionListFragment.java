14e65c890c046b54e01911e1dd3f57324ffbc804
Shahanur <shahanur@surecash.net>
2018-03-22 11:32:08 +0600

    Add swipe refresh in Transaction List

--- app/src/main/java/com/sample/mosbysample/fragment/TransactionListFragment.java
+++ app/src/main/java/com/sample/mosbysample/fragment/TransactionListFragment.java
@@ -2,17 +2,22 @@ package com.sample.mosbysample.fragment;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.hannesdorfmann.mosby3.mvp.MvpFragment;
 
 import java.util.ArrayList;
 import java.util.List;
+
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.schedulers.Schedulers;
+
 import com.sample.mosbysample.R;
 import com.sample.mosbysample.adapter.TransactionListAdapter;
 import com.sample.mosbysample.model.TransactionModel;
@@ -27,7 +32,9 @@ import com.sample.mosbysample.mvp.transaction.TransactionListPresenterImp;
 
 public class TransactionListFragment extends MvpFragment<TransactionListContract.View, TransactionListContract.Presenter> implements TransactionListContract.View {
     ListView listView;
+
     TransactionListAdapter adapter;
+    SwipeRefreshLayout refreshLayout;
 
     @Override
     public TransactionListContract.Presenter createPresenter() {
@@ -39,6 +46,8 @@ public class TransactionListFragment extends MvpFragment<TransactionListContract
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_transaction_list, container, false);
         listView = (ListView) view.findViewById(R.id.list_view);
+        refreshLayout = (SwipeRefreshLayout) view.findViewById(R.id.swipe_refresh);
+        refreshLayout.setOnRefreshListener(() -> getPresenter().loadData());
         return view;
     }
 
@@ -52,21 +61,25 @@ public class TransactionListFragment extends MvpFragment<TransactionListContract
 
     @Override
     public void startLoad() {
-
+        if (!refreshLayout.isRefreshing()) {
+            refreshLayout.setRefreshing(true);
+        }
     }
 
     @Override
     public void success(SuccessViewModel result) {
-
+        refreshLayout.setRefreshing(false);
     }
 
     @Override
     public void error(ErrorViewModel result) {
-
+        refreshLayout.setRefreshing(false);
+        Toast.makeText(getActivity(), "Error happened", Toast.LENGTH_LONG).show();
     }
 
     @Override
     public void showData(List<TransactionModel> modelList) {
+        adapter.clear();
         adapter.addAll(modelList);
         adapter.notifyDataSetChanged();
     }
